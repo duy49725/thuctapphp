443658cf2acfb66df45f7e70f0c25ba898b18abe
duy49725 <duy49725@gmail.com>
2025-04-02 11:25:09 +0700

    themcheckemptythemcheckempty

--- app/views/users/testcreate.php
+++ app/views/users/testcreate.php
@@ -2,38 +2,38 @@
     <p>Thêm mới người dùng<p>
     <form action="<?php echo BASE_URL; ?>/public/index.php?url=users/testcreate" method="POST">
         <div class="input-container" style="<?php echo  $username_err ? 'margin-bottom: 70px' : 'margin-bottom: 40px'; ?>">
-            <div class="form-container">
-                <label for="">Tên đăng nhập</label>
-                <input id="username" type="text" name="username" value="<?php echo $username; ?>">
+            <div class="form-container" style="position: relative">
+                <label for="">Tên đăng nhập<span style="color: red; position: absolute; top: 4px" id="check-username-empty">*</span></label>
+                <input id="username" type="text" name="username" value="<?php echo $username; ?>" onkeyup="checkUsername()">
             </div>
             <div class="check check-username" ><?php echo $username_err; ?></div>
         </div>
         <div class="input-container" style="<?php echo  $fullname_err ? 'margin-bottom: 70px' : 'margin-bottom: 40px'; ?>">
-            <div class="form-container">
-                <label for="">Tên người dùng</label>
-                <input id="fullname" type="text" name="fullname" value="<?php echo $fullname; ?>">
+            <div class="form-container" style="position: relative">
+                <label for="">Tên người dùng<span id="check-fullname-empty" style="color: red; position: absolute; top: 4px">*</span></label>
+                <input id="fullname" type="text" name="fullname" value="<?php echo $fullname; ?>" onkeyup="checkFullname()">
             </div>
             <span class="check check-fullname"><?php echo $fullname_err; ?></span>
         </div>
         <div class="input-container" style="<?php echo  $password_err ? 'margin-bottom: 70px' : 'margin-bottom: 40px'; ?>">
-            <div class="form-container">
-                <label for="">Mật khẩu</label>
-                <input id="password" type="password" name="password" value="<?php echo $password; ?>">
+            <div class="form-container" style="position: relative">
+                <label for="">Mật khẩu<span id="check-password-empty" style="color: red; position: absolute; top: 4px">*</span></label>
+                <input id="password" type="password" name="password" value="<?php echo $password; ?>" onkeyup="checkPassword()">
             </div>
             <span class="check check-password"><?php echo $password_err; ?></span>
         </div>
         <div class="input-container" style="<?php echo  $email_err ? 'margin-bottom: 70px' : 'margin-bottom: 40px'; ?>">
-            <div class="form-container">
-                <label for="">Email</label>
-                <input id="email" type="email" name="email" value="<?php echo $email; ?>">
+            <div class="form-container" style="position: relative">
+                <label for="">Email<span id="check-email-empty" style="color: red; position: absolute; top: 4px">*</span></label>
+                <input id="email" type="email" name="email" value="<?php echo $email; ?>" onkeyup="checkEmail()">
             </div>
             <span class="check check-email"><?php echo $email_err; ?></span>
         </div>
         <div class="input-container"> 
-            <div class="form-container">
-                <label for="">Ngày sinh</label>
+            <div class="form-container" style="position: relative">
+                <label for="">Ngày sinh<span id="check-birthDate-empty" style="color: red; position: absolute; top: 4px">*</span></label>
                 <div class="date-container">
-                    <input id="datetime" value="<?php echo $birthDate; ?>" onchange="changeDate()" type="date" name="birthDate" style="width: 189px; height: 40px;  padding: 5px;">
+                    <input id="datetime" value="<?php echo $birthDate; ?>" onchange="changeDate(); checkBirthDate()" type="date" name="birthDate" style="width: 189px; height: 40px;  padding: 5px;">
                 </div>
             </div>
         </div>
@@ -100,6 +100,51 @@
             datetime.style.color = 'black';
         }else{
             datetime.style.color = 'transparent';
+        }
+    }
+    function checkUsername(){
+        const username = document.getElementById('username').value;
+        const check_username_empty = document.getElementById('check-username-empty');
+        if(username){
+            check_username_empty.style.display = 'none';
+        }else{
+            check_username_empty.style.display = 'inline';
+        }
+    }
+    function checkFullname(){
+        const fullname = document.getElementById('fullname').value;
+        const check_fullname_empty = document.getElementById('check-fullname-empty');
+        if(fullname){
+            check_fullname_empty.style.display = 'none';
+        }else{
+            check_fullname_empty.style.display = 'inline';
+        }
+    }
+    function checkPassword(){
+        const password = document.getElementById('password').value;
+        const check_password_empty = document.getElementById('check-password-empty');
+        if(password){
+            check_password_empty.style.display = 'none';
+        }else{
+            check_password_empty.style.display = 'inline';
+        }
+    }
+    function checkEmail(){
+        const email = document.getElementById('email').value;
+        const check_email_empty = document.getElementById('check-email-empty');
+        if(email){
+            check_email_empty.style.display = 'none';
+        }else{
+            check_email_empty.style.display = 'inline';
+        }
+    }
+    function checkBirthDate(){
+        const birthDate = document.getElementById('datetime').value;
+        const check_birthDate_empty = document.getElementById('check-birthDate-empty');
+        if(birthDate){
+            check_birthDate_empty.style.display = 'none';
+        }else{
+            check_birthDate_empty.style.display = 'inline';
         }
     }
     // document.querySelector('form').addEventListener('submit', function(e){
